e92c8201de792129399cae4723774744f5cd51cf
Се́рхио <s.golushko@freematiq.com>
2025-03-19 12:25:19 +0700

    1

---
+++ src/RBX/request/ReferenceCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RBX\request;
+
+use RBX\response\dto\reference\CurrencyListDto;
+
+class ReferenceCollection extends BaseRequest
+{
+    const PATH_CURRENCY_LIST = 'reference/currency-list';
+
+    /**
+     * @return CurrencyListDto
+     * @throws \Exception
+     */
+    public function getBalance(): CurrencyListDto
+    {
+        $response = $this->execute(self::PATH_CURRENCY_LIST, 'GET');
+        $result = new CurrencyListDto();
+        $result->parseApiResponse($response);
+
+        return $result;
+    }
+}
